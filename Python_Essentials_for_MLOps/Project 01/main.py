6b678f20187ba44b9910156868532cf06c828059
Matheus Diniz <dinizmaths@gmail.com>
2023-10-08 13:12:19 -0300

    create tests and fix code using pylint

--- Python_Essentials_for_MLOps/Project 01/main.py
+++ Python_Essentials_for_MLOps/Project 01/main.py
@@ -2,18 +2,17 @@
 
 import sys
 import os
-import re
 import zipfile
 import logging
 import requests
 
-import numpy as np
 import pandas as pd
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 from tqdm import tqdm
+
+from utils import clean_title, find_similar_movies
 
 
 logging.basicConfig(level=logging.INFO)
@@ -82,85 +81,13 @@ SIZE = int(sys.argv[sys.argv.index("--size") + 1]) if "--size" in sys.argv else 
 movies  = pd.read_csv("./data/ml-25m/movies.csv")
 ratings = pd.read_csv("./data/ml-25m/ratings.csv")
 
-def clean_title(title: str) -> str:
-    """
-    Remove alphanumeric characters and spaces from a string.
-
-    Args:
-        title (str): The string to be processed.
-
-    Returns:
-        str: The resulting string after removing the characters.
-    """
-
-    return re.sub("[^a-zA-Z0-9 ]", "", title)
-
 movies["clean_title"] = movies["title"].apply(clean_title)
 vectorizer = TfidfVectorizer(ngram_range=(1, 2))
 tfidf = vectorizer.fit_transform(movies["clean_title"])
 
-def search(title: str) -> pd.DataFrame:
-    """
-    Search for similarity scores between the given title and a set of titles represented as vectors.
+tfidf_vectorizer = {
+    "vectorizer": vectorizer,
+    "tfidf": tfidf
+}
 
-    Args:
-        title (str): The input title to compare against a set of titles.
-
-    Returns:
-        pd.DataFrame: A dataframe containing the top 5 most similar titles.
-    """
-
-    title = clean_title(title)
-    query_vec = vectorizer.transform([title])
-    similarity = cosine_similarity(tfidf, query_vec).flatten()
-    indices = np.argpartition(similarity, -SIZE)[-SIZE:]
-    results = movies.iloc[indices][::-1]
-
-    return results
-
-def find_similar_movies(movie_title: str) -> pd.DataFrame:
-    """
-    Find similar movies based on the given movie ID.
-
-    Args:
-        movie_id (int): The ID of the movie to find similar movies for.
-
-    Returns:
-        pd.DataFrame: A dataframe containing the top 10 most similar movies.
-    """
-
-    search_results = search(movie_title)
-    movie_id = search_results.iloc[0]["movieId"]
-
-    similar_users = ratings[
-        (ratings["movieId"] == movie_id) &
-        (ratings["rating"] > 4)
-    ]["userId"].unique()
-
-    similar_user_recs = ratings[
-        (ratings["userId"].isin(similar_users)) &
-        (ratings["rating"] > 4)
-    ]["movieId"]
-
-    similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
-
-    similar_user_recs = similar_user_recs[similar_user_recs > .10]
-
-    all_users = ratings[
-        (ratings["movieId"].isin(similar_user_recs.index)) &
-        (ratings["rating"] > 4)
-    ]
-
-    all_user_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
-
-    rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
-    rec_percentages.columns = ["similar", "all"]
-
-    rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
-    rec_percentages = rec_percentages.sort_values("score", ascending=False)
-
-    return rec_percentages.head(10).merge(
-        movies, left_index=True, right_on="movieId"
-    )[["score", "title", "genres"]]
-
-print(find_similar_movies(MOVIE_TITLE))
+print(find_similar_movies(MOVIE_TITLE, movies, ratings, tfidf_vectorizer, SIZE))
